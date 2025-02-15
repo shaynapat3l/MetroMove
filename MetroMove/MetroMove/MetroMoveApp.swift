95b96035b655819407aa5c4c822611dafef522fe
Sanjana Nukala <nsanjana@ad.unc.edu>
2025-02-15 13:01:20 -0500

    Initial Commit

---
+++ MetroMove/MetroMove/MetroMoveApp.swift
@@ -0,0 +1,17 @@
+//
+//  MetroMoveApp.swift
+//  MetroMove
+//
+//  Created by Sanjana Nukala on 2/15/25.
+//
+
+import SwiftUI
+
+@main
+struct MetroMoveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
